c7853e5520b9d5071bdcb68a4167f8946b0ac826
Luiz Assumpção <luizrafael.apneto@gmail.com>
2026-09-10 22:06:10 -0300

    Initial project setup

---
+++ src/main/java/com/luiz/secure_user_api/SecureUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.secure_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz/secure_user_api/SecureUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.secure_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
